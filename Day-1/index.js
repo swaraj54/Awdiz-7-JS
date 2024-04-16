26537774d162d884d6d373bffd9568984654734a
Swaraj Jadhav <jadhavswaraj1920@gmail.com>
2024-04-16 21:14:52 +0530

    Updates

---
+++ Day-1/index.js
@@ -0,0 +1,116 @@
+// 1. Data 
+
+// data is information of anything - ali , awdiz, 
+
+
+// 2. Datatypes 
+
+// name - ali - string
+// age  - 20 -  number 
+// yes/no     - boolean 
+// confused  - undefined
+
+
+// ali
+// 8765
+// hii 
+// welcome 
+// log out
+
+// 1. String  
+
+// syntax - rules 
+// "", '' , ``
+// kuchbhi  
+// kuchbhi 
+// "kuchbhi igvfybnhawd " -> string 
+// 'welcome' -> string 
+// `logout`  -> string 
+
+
+// 2. number 
+
+// syntax 
+// 12 
+// 123
+// 3423
+// 121
+// 4232
+
+// 3. Boolean  
+
+// syntax  
+
+// true false 
+
+// 4. undefined 
+
+// syntax 
+
+
+// Varibles 
+
+// storage 
+
+// var let const 
+
+// cupboard clothing = shirt
+
+var myStudentName = "ali" 
+var myStudentAge = 20
+var myStudentCompletedAssignment = true
+
+console.log(myStudentName)
+console.log(myStudentAge)
+console.log(myStudentCompletedAssignment)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
