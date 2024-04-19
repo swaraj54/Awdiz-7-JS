f0850f2769d12a09526e5b29e92953c8ed1d8718
Swaraj Jadhav <jadhavswaraj1920@gmail.com>
2024-04-19 20:53:59 +0530

    Updates

---
+++ Day-4/Index.js
@@ -0,0 +1,84 @@
+// Datatypes
+// String
+// Number
+// Boolean
+// undefined
+// console.log()
+// typeof()
+// Varibles
+// Var
+// let
+// const
+// Scope
+// block
+// hoisting
+
+// Conditional Statement
+// 1. if else
+// 2. switch
+
+// if else
+
+// syntax
+// if(condition){
+//     statement 1
+// } else {
+//     statement 2
+// }
+
+var age = 10;
+console.log(age > 18);
+
+if (age > 18) {
+  console.log("Your age is valid for Driving License.");
+} else if (age == 18) {
+  console.log("you are allowed for LL.");
+} else if (age == 10) {
+  console.log("Your age is 10.");
+} else {
+  console.log("Your age is Not valid for Driving License.");
+}
+
+// if(condition 1){
+// statement 1
+// } else if(condition 2) {
+// statement 2
+// } else {
+//  statement 3
+// }
+
+// if(condition age is greather tahn 18 ){
+// statement 1
+// } else {
+//     statement 2
+// }
+
+// Comparision Operator
+
+// value 1 > value 2
+// value 1 < value 2
+// value 1 <= value 2
+// value 1 >= value 2
+// =   -> to assign value to varible
+// ==  -> to compare only datas
+// === -> to compare with data and datatype
+
+var num1 = 10;
+var num2 = 10;
+console.log(num1 == num2);
+
+var num1 = 10;
+var num2 = '10';
+console.log(num1 == num2);
+console.log(num1 === num2);
+
+console.log(20 > 30);
+console.log(20 < 30);
+console.log(30 <= 30);
+console.log(30 >= 30);
+console.log(30 != 30);
+
+
+// Q. Given an number find out the number is even or odd. 
+// if number is even then print "Even" and if number is odd then print "Odd"
+// var number = 345; Use if else conditional statement .
