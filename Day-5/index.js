99be0119f1d5181776df3da810cd04ecac85de27
Swaraj Jadhav <jadhavswaraj1920@gmail.com>
2024-04-25 20:55:45 +0530

    Updates

--- Day-5/index.js
+++ Day-5/index.js
@@ -33,15 +33,18 @@
 // 2. Switch - conditional statement
 
 function CheckNumber() {
-  //   console.log(document.getElementById("number").value);
+  // console.log(document.getElementById("number").value);
   //   alert("Function called.");
   const number = document.getElementById("number").value;
   console.log(number, "number");
   if (!number) {
+    console.log("Please type the number.");
     alert("Please type the number.");
   } else if (number % 2 === 0) {
+    console.log(`Number ${number} is Even.`);
     alert(`Number ${number} is Even.`);
   } else {
+    console.log(`Number ${number} is Odd.`);
     alert(`Number ${number} is Odd.`);
   }
 }
---
+++ Day-6/index.js
@@ -0,0 +1,68 @@
+// Datatypes
+// String
+// Number
+// Boolean
+// undefined
+// console.log()
+// typeof()
+// Varibles
+// Var
+// let
+// const
+// Scope
+// block
+// hoisting
+
+// Conditional Statement
+// 1. if else
+// 2. switch
+
+// 2. Switch - conditional statement
+
+// switch(condition, 11){
+//     case first 10  :
+//     statement 1
+//     case second 11:
+//     statement 2
+//     case third 12:
+//     statement 3
+//     default
+//     statement
+// }
+
+var fruit = "HBHGVJ";
+
+switch (fruit) {
+  case "Watermelon":
+    console.log("The fruit is watermelon.");
+    break;
+  case "Mango":
+    console.log("The fruit is Mango.");
+    break;
+  case "Banana":
+    console.log("The fruit is Banana.");
+    break;
+  default:
+    console.log("Fruit not exist.");
+}
+
+// && || Operators
+
+// && Logical Operator :
+
+var first = 10;
+var second = 20;
+var third = 30;
+
+console.log(first > second);
+console.log(first > third);
+console.log(second > third);
+
+console.log(30 > 20 && 20 > 10 && 3 > 10); //true && true && true ->
+// if all values are true then only && operator will return true
+// if even one value found false then && operator will return false
+
+console.log(3 < 2 || 3 < 1 || 2 < 1); // true || true || true 
+// if even one true found then || operator will return true 
+// when all conditions are false then only it'll return false
+
