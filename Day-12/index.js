363ade710fbe832bbf5a79806406701b3246dbe0
Swaraj Jadhav <jadhavswaraj1920@gmail.com>
2024-05-08 21:14:36 +0530

    Updates

---
+++ Day-12/index.js
@@ -0,0 +1,62 @@
+// Datatypes :
+// String  - "" '' `` , template literals `My name ${name}.`
+// Number - 20 1
+// Boolean  - true false
+// Undefined
+
+// Varibles - to store data
+// Var
+// Let
+// Const
+// Redelcare and reassign
+// Scope
+// HOISTING
+
+// Conditional Statement
+// if else
+// switch
+
+// Loops
+// for
+// for(starting; ending ; sequence ){
+// }
+// while - pending
+
+// Comparision Operator
+// > < <= >= !=
+// = == ===
+// || &&
+
+// function
+// Traditional
+// Arrow
+
+// Array
+// []
+// Push pop s spl shift unshift
+// Map , foreach , reduce - pending
+
+// Nest For loop
+// for(){
+//     for(){
+//         for(){
+
+//         }
+//     }
+// }
+
+// Object
+// syntax
+// access
+// loop
+// array - object -> assign - pending
+
+// document.getelement
+
+// var array = [1];
+// array[2] = 2;
+// console.log(array);
+
+// var myObject = { name: "Awdiz" };
+// myObject["Age"] = 10;
+// console.log(myObject);
