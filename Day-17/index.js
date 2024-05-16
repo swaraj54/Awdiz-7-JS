2d255214bfca9a0c565b07f56fef4abae821c4ec
Swaraj Jadhav <jadhavswaraj1920@gmail.com>
2024-05-16 21:05:38 +0530

    Updates

---
+++ Day-17/index.js
@@ -0,0 +1,22 @@
+// Promise
+function getData() {
+  // console.log("Inside function")
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      const result = true; // backend api call ->  false
+      if (result) {
+        resolve("Data successfully recevied.");
+      } else {
+        reject("Failed.");
+      }
+    }, 4000);
+  });
+}
+
+getData()
+  .then((response) => {
+    console.log(response, "response");
+  })
+  .catch((error) => {
+    console.error(error, "error");
+  });
